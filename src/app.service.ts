8e8b83aee9226f74529275a8dc6b80f19f064e25
Julio Corbaz <julio.corbaz@gmail.com>
2024-11-25 11:07:06 -0300

    build 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'julio.corbaz@gmail.com! - JCC';
+    return 'julio.corbaz@gmail.com - JCC';
   }
 }
