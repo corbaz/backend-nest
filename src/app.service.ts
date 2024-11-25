65273b98ced239c520493be568b096455fa65a2e
Julio Corbaz <julio.corbaz@gmail.com>
2024-11-25 12:19:55 -0300

    2024

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'julio.corbaz@gmail.com - JCC';
+    return 'La Casa de la Pc - 2024';
   }
 }
